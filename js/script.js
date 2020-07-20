5d492a64eee1de75d9ba6f2c93e119904553a776
Maria P. Campos <mpfc.maria@gmail.com>
2020-07-20 11:42:55 -0300

    creating the movement

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// -------- GLOBAL VARIABLES---------
+
 let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 let box = 32;
@@ -6,7 +8,11 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+let game = setInterval(startGame, 100);
 
+
+// -------- FUNCTIONS ---------
 function createBG(){
     context.fillStyle = 'lightgreen';
     context.fillRect(0,0, 16* box, 16* box);
@@ -20,6 +26,27 @@ function createSnake(){
 
 }
 
+function  startGame(){
+    createBG();
+    createSnake();
 
-createBG();
-createSnake();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == 'right') snakeX += box;
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box;
+    if(direction == 'down') snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+
+// -------------------- 
